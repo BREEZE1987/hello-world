0873d6c418665891ac4f93dfc0ddefddb911ef60
BREEZE <tanjingrong_123@163.com>
2018-10-17 11:15:58 +0800

    Test diff2

--- nuxustest/src/main/java/com/breeze/nuxustest/Test.java
+++ nuxustest/src/main/java/com/breeze/nuxustest/Test.java
@@ -6,6 +6,6 @@ public class Test {
 		System.out.println("Hello World!");
 		System.out.println("Test again!");
 		System.out.println("Test commit to local repository !");
-		
+		System.out.println("Test diff2");
 	}
 }
